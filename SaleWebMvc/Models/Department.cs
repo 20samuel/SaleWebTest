ea1f2cb84a1c504b32c545288b90ce57c9076f0b
Samuel Paes <samuelpaesmedeiros@gmail.com>
2022-01-22 08:56:47 -0400

    Other entities and second migration

--- SaleWebMvc/Models/Department.cs
+++ SaleWebMvc/Models/Department.cs
@@ -1,9 +1,31 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace SaleWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public void AddSeller(Seller sl)
+        {
+            Sellers.Add(sl);
+        }
+        public double TotalSellers(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seler => seler.TotalSales(initial, final));
+        }
     }
 }
---
+++ SaleWebMvc/Models/Enums/SaleStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace SaleWebMvc.Models.Enums
+{
+    public enum SaleStatus:int
+    {
+        PENDING =0,
+        BILLED = 1,
+        CANCELED = 2
+    }
+}
